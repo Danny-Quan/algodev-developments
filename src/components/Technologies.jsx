cb19d546dfc9efc95cbf46a4eeb9090e155834a6
DANNY QUAN <danielsah118@gmail.com>
2023-02-19 11:28:00 +0000

    first commit

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -9,7 +9,7 @@ function Technologies() {
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis labore
         laudantium quam quas iste? Temporibus nemo rerum sunt impedit, quae quas
-        eaque similique ut mollitia sint, eligendi odit cum distinctio.
+        eaque similique ut mollitia sint, eligendi odit cum.
       </p>
     </div>
     </section>
